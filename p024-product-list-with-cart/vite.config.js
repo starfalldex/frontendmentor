fe5848b48d9583d84aa5273f4711ff8ed0f25345
dex-sleep <deepakgobaricode@gmail.com>
2025-03-06 21:12:22 +0530

    bug fix p024

--- p024-product-list-with-cart/vite.config.js
+++ p024-product-list-with-cart/vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/practice_frontendmentor/p024-product-list-with-cart/'
+  base: '/'
 });
